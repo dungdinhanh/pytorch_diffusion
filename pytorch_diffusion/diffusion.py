1a9096a3dabf5dfe31efe5e9d0d12d794391b840
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2022-07-29 13:28:49 +1000

    save change

--- pytorch_diffusion/diffusion.py
+++ pytorch_diffusion/diffusion.py
@@ -3,6 +3,7 @@ import torch
 
 from pytorch_diffusion.model import Model
 from pytorch_diffusion.ckpt_util import get_ckpt_path
+import os
 
 
 def get_beta_schedule(beta_schedule, *, beta_start, beta_end, num_diffusion_timesteps):
@@ -273,8 +274,13 @@ if __name__ == "__main__":
     name = sys.argv[1] if len(sys.argv)>1 else "cifar10"
     bs = int(sys.argv[2]) if len(sys.argv)>2 else 1
     nb = int(sys.argv[3]) if len(sys.argv)>3 else 1
+    save_folder = str(sys.argv[4]) if len(sys.argv) > 4 else "results"
+
     diffusion = Diffusion.from_pretrained(name)
     for ib in tqdm.tqdm(range(nb), desc="Batch"):
         x = diffusion.denoise(bs, progress_bar=tqdm.tqdm)
         idx = ib*bs
-        diffusion.save(x, "results/"+name+"/{:06}.png", start_idx=idx)
+
+        image_path = os.path.join(save_folder, name+"/{:06}.png")
+
+        diffusion.save(x, image_path, start_idx=idx)
