e450bd968f47e5402268b09e16edc7ba41dfe64b
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2022-08-01 12:19:32 +1000

    adding load cifar10

---
+++ load_cifar10.py
@@ -0,0 +1,11 @@
+import torchvision.datasets as datasets
+import os
+
+cifar_testset = datasets.CIFAR10(root='./temp_data', train=False, download=True, transform=None)
+
+folder = "temp_data/cifar10_test"
+
+os.makedirs(folder, exist_ok=True)
+for i in range(len(cifar_testset)):
+    img, target = cifar_testset[i]
+    img.save(os.path.join(folder, "IMG{:06}.png".format(i)))
