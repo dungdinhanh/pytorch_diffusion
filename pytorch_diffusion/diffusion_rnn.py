df52da41fe5bade3723a93c71771fb4bd35d6a00
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2022-08-06 15:36:14 +1000

    save change

--- pytorch_diffusion/diffusion_rnn.py
+++ pytorch_diffusion/diffusion_rnn.py
@@ -13,6 +13,7 @@ class DiffusionRNN(Diffusion):
 
         emb_res, emb_channel = self.model.emb_res, self.model.emb_channel
         self.model_rnn = ModelRNN(emb_res, emb_channel)
+        self.model_rnn.to(device)
         if train:
             self.model_rnn.train()
         else:
