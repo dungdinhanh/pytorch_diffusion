22588f3fd6f06721f73e3c5ae04c4b83a80b3105
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2022-08-18 17:35:53 +1000

    save change

--- pytorch_diffusion/diffusion_rnn_isolate_iter.py
+++ pytorch_diffusion/diffusion_rnn_isolate_iter.py
@@ -5,88 +5,15 @@ import time
 from   datasets.data_helper import *
 import random
 from torch.utils.tensorboard import SummaryWriter
+from pytorch_diffusion.diffusion_rnn import *
 
 
-def denoising_step_rnn(model_sc_output, x, t,*,
-                   logvar,
-                   sqrt_recip_alphas_cumprod,
-                   sqrt_recipm1_alphas_cumprod,
-                   posterior_mean_coef1,
-                   posterior_mean_coef2,
-                   return_pred_xstart=False):
-    """
-    Sample from p(x_{t-1} | x_t)
-    """
-    # instead of using eq. (11) directly, follow original implementation which,
-    # equivalently, predicts x_0 and uses it to compute mean of the posterior
-    # 1. predict eps via model
-    model_output = model_sc_output
-    # 2. predict clipped x_0
-    # (follows from x_t=sqrt_alpha_cumprod*x_0 + sqrt_one_minus_alpha*eps)
-    pred_xstart = (extract(sqrt_recip_alphas_cumprod, t, x.shape)*x -
-                   extract(sqrt_recipm1_alphas_cumprod, t, x.shape)*model_output)
-    pred_xstart = torch.clamp(pred_xstart, -1, 1)
-    # 3. compute mean of q(x_{t-1} | x_t, x_0) (eq. (6))
-    mean = (extract(posterior_mean_coef1, t, x.shape)*pred_xstart +
-            extract(posterior_mean_coef2, t, x.shape)*x)
-
-    logvar = extract(logvar, t, x.shape)
-
-    # sample - return mean for t==0
-    noise = torch.randn_like(x)
-    mask = 1-(t==0).float()
-    mask = mask.reshape((x.shape[0],)+(1,)*(len(x.shape)-1))
-    sample = mean + mask*torch.exp(0.5*logvar)*noise
-    sample = sample.float()
-    if return_pred_xstart:
-        return sample, mean, pred_xstart
-    return sample, mean
-
-
-def avg_accumulate(h_emb_cal, n, h_emb):
-    return (h_emb_cal + h_emb)/(n+1), n+1
-
-
-def compare_state_dict(model1: torch.nn.Module, model2: torch.nn.Module):
-    state_dict1 = model1.state_dict()
-    state_dict2 = model2.state_dict()
-    count = 0
-    for key in state_dict1.keys():
-        print(state_dict2[key] - state_dict1[key])
-        if count > 5:
-            break
-    pass
-
-
-class DiffusionRNN(Diffusion):
+class DiffusionRNN_IsolateIter(DiffusionRNN):
     def __init__(self, diffusion_config, model_config, device=None, train=True,
                  lr=0.01, weight_decay=1e-4, data_loader=None, log_folder="./runs"):
-        super(DiffusionRNN, self).__init__(diffusion_config=diffusion_config,
-                                           model_config=model_config, device=device, extract_version=True)
-
-        emb_res, emb_channel = self.model.emb_res, self.model.emb_channel
-        self.model_rnn = ModelRNN(emb_res, emb_channel)
-        self.test_model = ModelRNN(emb_res, emb_channel)
-        self.model_rnn.to(self.device)
-        self.test_model.to(self.device)
-        if train:
-            self.model_rnn.train()
-        else:
-            self.model_rnn.eval()
-
-        if train:
-            self.data_loader, _ = get_cifar_loader(train=train, test=False)
-        else:
-            _, self.data_loader = get_cifar_loader(train=train, test=True)
-
-        self.optimizer = optim.SGD(self.model_rnn.parameters(), lr=lr, weight_decay=weight_decay) # use SGD first
-        # will change later to see performance
-        self.loss_function = torch.nn.MSELoss(reduction='mean')
-        self.tensorboard_writer = SummaryWriter(os.path.join(log_folder, "log"))
-        self.log_folder = log_folder
-        self.folder_path = os.path.join(self.log_folder, "models")
-        self.start_iter = None
-        os.makedirs(self.folder_path, exist_ok=True)
+        super(DiffusionRNN_IsolateIter, self).__init__(diffusion_config, model_config, device, train, lr,
+                                                       weight_decay, data_loader, log_folder)
+        self.num_timesteps = 20
 
     def training(self, n, number_of_iters=10000):
         self.model_rnn.train()
@@ -96,7 +23,7 @@ class DiffusionRNN(Diffusion):
         for i in range(self.start_iter, number_of_iters, 1):
 
             # rand_number_timesteps = random.randint(5, self.num_timesteps-1)
-            rand_number_timesteps = random.randint(5 , 20)
+            rand_number_timesteps = random.randint(5 , 10)
             start_step = random.randint(1, self.num_timesteps - rand_number_timesteps - 1)
             stop_step = start_step + rand_number_timesteps - 1
 
@@ -183,46 +110,19 @@ class DiffusionRNN(Diffusion):
                                                                     posterior_mean_coef1=self.posterior_mean_coef1,
                                                                     posterior_mean_coef2=self.posterior_mean_coef2,
                                                                     return_pred_xstart=True)
-
-                    # RNN accumulate
-                    if h_emb_accumulate is None:
-                        h_emb_accumulate = torch.zeros_like(h_emb).to(self.device)
-                        count_accumulate = 0
-
-                    h_emb_accumulate, count_accumulate = avg_accumulate(h_emb_accumulate, count_accumulate, h_emb)
-                    c_h_emb_accumulate = c_rnn + torch.rand_like(h_emb_accumulate) * h_emb_accumulate
-
-                    accumulate_x_prime = self.model_rnn.forward_dec(c_h_emb_accumulate)
-
-                    model_accumulate_output = self.model.forward_up(accumulate_x_prime, hs_0, temb_0)
-                    sample_accumulate, mean_accumulate, xpred_accumulate = denoising_step_rnn(
-                                                                    model_accumulate_output,
-                                                                    x=x_0,
-                                                                    t=t,
-                                                                    logvar=self.logvar,
-                                                                    sqrt_recip_alphas_cumprod=
-                                                                    self.sqrt_recip_alphas_cumprod,
-                                                                    sqrt_recipm1_alphas_cumprod=
-                                                                    self.sqrt_recipm1_alphas_cumprod,
-                                                                    posterior_mean_coef1=self.posterior_mean_coef1,
-                                                                    posterior_mean_coef2=self.posterior_mean_coef2,
-                                                                    return_pred_xstart=True)
-
                     while len(hs) != 0:
                         hs.pop()
 
                     loss_iter = self.loss_function(mean_rnn, mean)
-                    loss_accumulate = self.loss_function(mean_accumulate, mean)
                 x = sample
             self.optimizer.zero_grad()
-            final_loss = loss_iter + loss_accumulate
+            final_loss = loss_iter
             final_loss.backward()
             self.optimizer.step()
 
             print(final_loss.item())
             self.tensorboard_writer.add_scalar("Loss/train", final_loss.item(), i)
             self.tensorboard_writer.add_scalar("Loss/train_loss_iter", loss_iter.item(), i)
-            self.tensorboard_writer.add_scalar("Loss/train_loss_accumulate", loss_accumulate.item(), i)
             if i % 10 == 0:
                 self.tensorboard_writer.flush()
             if i % 100 == 0 or i == number_of_iters - 1:
@@ -355,7 +255,6 @@ class DiffusionRNN(Diffusion):
                 diffusion.start_iter = state['iter']
             diffusion.model_rnn.to(diffusion.device)
         return diffusion
-
 
     def denoise(self, n, n_steps=None, x=None, curr_step=None,
                 progress_bar=lambda i, total=None: i,
