7ea4d5eb9d13c37acd053c988443324a519d99f0
Anh-Dung <dinhanhdung1996@gmail.com>
2022-09-02 16:43:26 +1000

    fix bug

--- pytorch_diffusion/model.py
+++ pytorch_diffusion/model.py
@@ -343,9 +343,7 @@ class ModelExtract(Model):
 
 
     def forward_down_mid(self, x, t):
-        print(x.shape[2])
-        print(x.shape[3])
-        print(self.resolution)
+        print(x.shape)
         assert x.shape[2] == x.shape[3] == self.resolution
 
         # timestep embedding
