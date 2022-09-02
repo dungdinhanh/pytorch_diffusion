0187c40ec329d1a92c3da9b69ab323404f9d7866
Anh-Dung <dinhanhdung1996@gmail.com>
2022-09-02 17:56:38 +1000

    check block_in

--- pytorch_diffusion/test/model_test.py
+++ pytorch_diffusion/test/model_test.py
@@ -29,7 +29,7 @@ class ModelReconstruct(nn.Module):
                                          out_channels=block_out,
                                          temb_channels=self.temb_ch,
                                          dropout=dropout))
-                print("%d - %d"%(i_block, self.block_in))
+                print("%d - %d - %d"%(i_level, i_block, self.block_in))
                 block_in = block_out
                 if curr_res in attn_resolutions:
                     attn.append(AttnBlock(block_in))
@@ -52,7 +52,7 @@ class ModelReconstruct(nn.Module):
         print("______________________")
         for i_level in reversed(range(self.num_resolutions)):
             for i_block in range(self.num_res_blocks + 1):
-                print(i_block)
+                print("%d - %d - %d"%(i_level, i_block, h.shape[1]))
                 h = self.up[i_level].block[i_block](h, temb)
                 if len(self.up[i_level].attn) > 0:
                     h = self.up[i_level].attn[i_block](h)
