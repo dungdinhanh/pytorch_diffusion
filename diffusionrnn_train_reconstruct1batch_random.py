a308ecf28afe37f4381691ee7bc34c7dec71dd57
Anh-Dung <dinhanhdung1996@gmail.com>
2022-09-08 11:20:49 +1000

    save change

--- diffusionrnn_train_reconstruct1batch_random.py
+++ diffusionrnn_train_reconstruct1batch_random.py
@@ -1,4 +1,4 @@
-from pytorch_diffusion.test.diffusion_reconstruct_1batch import *
+from pytorch_diffusion.test.diffusion_reconstruct_1batch_random import *
 import os
 
 if __name__ == "__main__":
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     os.makedirs(save_folder, exist_ok=True)
 
-    diffusion = DiffusionReconstruct1Batch.from_pretrained(name, log_folder=save_folder, state_path=rnn_state, bs=bs)
+    diffusion = DiffusionReconstruct1BatchRandomInit.from_pretrained(name, log_folder=save_folder, state_path=rnn_state, bs=bs)
 
     diffusion.training(bs, nb)
 
