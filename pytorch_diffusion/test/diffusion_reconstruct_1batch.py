97fba88b2b55bff3babf3787ed4e1290a52f9cd0
Anh-Dung <dinhanhdung1996@gmail.com>
2022-09-08 10:56:42 +1000

    save change

--- pytorch_diffusion/test/diffusion_reconstruct_1batch.py
+++ pytorch_diffusion/test/diffusion_reconstruct_1batch.py
@@ -14,7 +14,7 @@ from pytorch_diffusion.test.diffusion_reconstruct import *
 
 class DiffusionReconstruct1Batch(DiffusionReconstruct):
     def __init__(self, diffusion_config, model_config, device=None, train=True,
-                 lr=0.001, weight_decay=1e-4, data_loader=None, log_folder="./runs", bs=64, load_plain_image=False):
+                 lr=0.01, weight_decay=1e-4, data_loader=None, log_folder="./runs", bs=64, load_plain_image=False):
         super(DiffusionReconstruct1Batch, self).__init__(diffusion_config, model_config, device, train, lr,
                                                          weight_decay,
                                                          data_loader, log_folder, bs, load_plain_image)
